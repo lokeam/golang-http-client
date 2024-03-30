b0d418f96678b7ca87d3306114a8c07480e93679
Ahn Ming Loke <lokeahnming@gmail.com>
2024-03-30 13:05:01 -0400

    feat: create http client public interface and testing example

--- gohttp/client.go
+++ gohttp/client.go
@@ -1,5 +1,26 @@
-package go_httpclient
+package gohttp
 
-type HttpClient struct {
+type httpClient struct {}
 
+func New() HttpClient {
+	client := &httpClient{}
+	return client
 }
+
+type HttpClient interface {
+	Get()
+	Post()
+	Put()
+	Patch()
+	Delete()
+}
+
+func(c *httpClient) Get() {}
+
+func(c *httpClient) Post() {}
+
+func(c *httpClient) Put() {}
+
+func(c *httpClient) Patch() {}
+
+func(c *httpClient) Delete() {}
