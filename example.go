63bf297fc980dd9b6e6d3cf76c9e26c61bed24f5
Ahn Ming Loke <lokeahnming@gmail.com>
2024-03-30 14:27:43 -0400

    feat: add custom and common headers, update example

--- example.go
+++ example.go
@@ -2,18 +2,43 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/lokeam/golang-http-client/gohttp"
 )
 
-func main() {
+var (
+	githubHttpClient = getGithubClient()
+)
+
+func getGithubClient() gohttp.HttpClient {
 	client := gohttp.New()
 
-	response, err := client.Get("https://api.github.com", nil)
+	commonHeaders := make(http.Header)
+	commonHeaders.Set("Authorization", "Some-auth-value-987")
+
+	client.SetHeaders(commonHeaders)
+	return client
+}
+
+func main() {
+	getUrls()
+	getUrls()
+	getUrls()
+}
+
+func getUrls() {
+	headers := make(http.Header)
+
+	response, err := githubHttpClient.Get("https://api.github.com", headers)
 
 	if (err != nil) {
 		panic(err)
 	}
 
 	fmt.Println(response.StatusCode)
+
+	bytes, _ := io.ReadAll(response.Body)
+	fmt.Println(string(bytes))
 }
--- gohttp/client_core.go
+++ gohttp/client_core.go
@@ -14,5 +14,29 @@ func (c * httpClient) do(method string, url string, headers http.Header, body in
 		return nil, errors.New("unable to generate a request")
 	}
 
+	allHeaders := c.getRequestHeaders(headers)
+	request.Header = allHeaders
+
 	return client.Do(request)
+}
+
+
+func(c *httpClient) getRequestHeaders(customHeaders http.Header) http.Header {
+	result := make(http.Header)
+
+	// Add common headers
+	for headerName, value := range c.Headers {
+		if len(value) > 0 {
+		  result.Set(headerName, value[0])
+		}
+	}
+
+	// Add custom headers
+	for headerName, value := range customHeaders {
+		if len(value) > 0 {
+		  result.Set(headerName, value[0])
+		}
+	}
+
+	return result
 }
