32f78e44d416fffba04447b7ef4b2bb48dfdce5d
Ahn Ming Loke <lokeahnming@gmail.com>
2024-03-31 18:45:30 -0400

    test: add tests for request body

--- gohttp/client_core_test.go
+++ gohttp/client_core_test.go
@@ -1,6 +1,7 @@
 package gohttp
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -22,4 +23,59 @@ func TestGetRequestHeaders(t *testing.T) {
 	if len(finalHeaders) != 3 {
 		t.Error("error: 3 headers expected")
 	}
+
+	if finalHeaders.Get("X-Request-Id") != "TEST-ABC" {
+		t.Error("invalid request id received")
+	}
+
+	if finalHeaders.Get("Content-Type") != "application/json" {
+		t.Error("invalid content type received")
+	}
+
+	if finalHeaders.Get("User-Agent") != "custom-http-client" {
+		t.Error("invalid user agent received")
+	}
+}
+
+
+func TestGetRequestBody(t* testing.T) {
+	client := httpClient{}
+	t.Run("WithoutBodyNilResponse", func(t* testing.T){
+		body, err := client.getRequestBody("", nil)
+
+		if err != nil {
+			t.Error("no error expected when passing nil body")
+		}
+
+		if body != nil {
+			t.Error("no body expected when passing nil body")
+		}
+	})
+
+	t.Run("BodyWithJson", func(t* testing.T) {
+		requestBody := []string{"one", "two"}
+
+		body, err := client.getRequestBody("application/json", requestBody)
+
+		fmt.Println(err)
+		fmt.Println(string(body))
+
+		if err != nil {
+			t.Error("no error expected when marshaling slice as json")
+		}
+
+		if string(body) != `["one","two"]` {
+			t.Error("invalid json body obtained")
+		}
+	})
+
+	t.Run("WithBodyWithXml", func(t* testing.T) {
+
+	})
+	t.Run("WithBodyWithJsonAsDefault", func(t* testing.T) {
+
+	})
+
+
+
 }
