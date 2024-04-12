35f025e4171251a9b2ad9aa17998368a7abe1e3d
Ahn Ming Loke <lokeahnming@gmail.com>
2024-04-12 17:02:30 -0400

    test: add mock server for endpoint testing, poc set tests to fail

--- gohttp/examples/get_test.go
+++ gohttp/examples/get_test.go
@@ -10,36 +10,74 @@ import (
 )
 
 func TestGetEndpoints(t *testing.T) {
+	gohttp.StartMockServer()
+
 	t.Run("TestErrorFetchingFromGithub", func(t *testing.T)  {
-		mock := gohttp.Mock{
+		gohttp.AddMock(gohttp.Mock{
 			Method: 	http.MethodGet,
 			Url: 			"https://api.github.com",
 			Error: 		errors.New("timeout getting github endpoints"),
-		}
+		})
 
 		endpoints, err := GetEndpoints()
+
+		if endpoints != nil {
+			t.Error("no endpoints expected")
+		}
+
+		if err == nil {
+			t.Error("error expected")
+		}
+
+		if err.Error() != "timeout getting github endpoints" {
+			t.Error("invalid error message received")
+		}
 	})
 
 	t.Run("TestErrorUnmarshallResponseBody", func(t *testing.T)  {
-		mock := gohttp.Mock{
+		gohttp.AddMock(gohttp.Mock{
 			Method: 							http.MethodGet,
 			Url: 									"https://api.github.com",
 			RequestBody: 					`{"current_user_url": 123}`,
 			ResponseStatusCode: 	http.StatusOK,
-		}
+		})
 
 		endpoints, err := GetEndpoints()
+
+		if endpoints != nil {
+			t.Error("no endpoints expected")
+		}
+
+		if err == nil {
+			t.Error("error expected")
+		}
+
+		if err.Error() != "json unmarshal error" {
+			t.Error("invalid error message received")
+		}
 	})
 
 	t.Run("TestNoError", func(t *testing.T)  {
-		mock := gohttp.Mock{
+		gohttp.AddMock(gohttp.Mock{
 			Method: 							http.MethodGet,
 			Url: 									"https://api.github.com",
 			RequestBody: 					`{"current_user_url": "https://api.github.com/user"}`,
 			ResponseStatusCode: 	http.StatusOK,
-		}
+		})
 
 		endpoints, err := GetEndpoints()
+
+		if err != nil {
+			t.Errorf("Unexpected error: '%s'", err.Error())
+		}
+
+		if endpoints == nil {
+			t.Error("nil returned when endpoints expected")
+		}
+
+		if endpoints.CurrentUser != "https://api.github.com/user" {
+			t.Error("invalid current user url")
+		}
 	})
 
 	endpoints, err := GetEndpoints()
