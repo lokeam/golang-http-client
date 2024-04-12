88cfe91810aeebae27bb99ff5336ea3a65c362ef
Ahn Ming Loke <lokeahnming@gmail.com>
2024-04-12 16:29:13 -0400

    test: create endpoint tests for fetching, unmarshaling json + no error

--- gohttp/examples/get_test.go
+++ gohttp/examples/get_test.go
@@ -1,11 +1,46 @@
 package examples
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
 	"testing"
+
+	"github.com/lokeam/golang-http-client/gohttp"
 )
 
-func TestGet(t *testing.T) {
+func TestGetEndpoints(t *testing.T) {
+	t.Run("TestErrorFetchingFromGithub", func(t *testing.T)  {
+		mock := gohttp.Mock{
+			Method: 	http.MethodGet,
+			Url: 			"https://api.github.com",
+			Error: 		errors.New("timeout getting github endpoints"),
+		}
+
+		endpoints, err := GetEndpoints()
+	})
+
+	t.Run("TestErrorUnmarshallResponseBody", func(t *testing.T)  {
+		mock := gohttp.Mock{
+			Method: 							http.MethodGet,
+			Url: 									"https://api.github.com",
+			RequestBody: 					`{"current_user_url": 123}`,
+			ResponseStatusCode: 	http.StatusOK,
+		}
+
+		endpoints, err := GetEndpoints()
+	})
+
+	t.Run("TestNoError", func(t *testing.T)  {
+		mock := gohttp.Mock{
+			Method: 							http.MethodGet,
+			Url: 									"https://api.github.com",
+			RequestBody: 					`{"current_user_url": "https://api.github.com/user"}`,
+			ResponseStatusCode: 	http.StatusOK,
+		}
+
+		endpoints, err := GetEndpoints()
+	})
 
 	endpoints, err := GetEndpoints()
 
